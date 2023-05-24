1f4ebe03ee22bf95da7eb8818f765a67eeecc92c
dzhumadil <dzhumadil.a@icloud.com>
2023-05-24 16:58:30 +0600

    Initial Commit

---
+++ products/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  products
+//
+//  Created by Абдулла-Бек on 24/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
